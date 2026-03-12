7981d055c03b38e4b6cdb6009be1a70543699234
bandrinithin24-ai <bandrinithin24@gmail.com>
2026-03-12 12:21:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ app.secret_key = os.environ.get("FLASK_SECRET_KEY", "generate-a-long-random-stri
 
 # ---------------- AWS CONNECTION ----------------
 # It is better to rely on EC2 IAM Roles than hardcoded regions if possible
-REGION = os.environ.get("AWS_REGION", "ap-south-1")
+REGION = os.environ.get("AWS_REGION", "us-east-1")
 dynamodb = boto3.resource('dynamodb', region_name=REGION)
 sns = boto3.client('sns', region_name=REGION)
 
 users_table = dynamodb.Table('travel-Users')
 bookings_table = dynamodb.Table('Bookings')
 
-SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:503561414488:BookingConfirmation"
+SNS_TOPIC_ARN = "arn:aws:sns:us-east-1:850995548830:BookingConfirmation"
 
 # ---------------- STATIC DATA ----------------
 bus_data = [
@@ -196,5 +196,6 @@ def logout():
 if __name__ == '__main__':
     # Running on 0.0.0.0 for EC2 access, but debug is OFF for safety
     app.run(host='0.0.0.0', port=5000, debug=False)
+
 
 

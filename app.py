e880f73c11b0b8a0ca9c84c6dbab7294082b12eb
bandrinithin24-ai <bandrinithin24@gmail.com>
2026-03-12 12:57:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -195,7 +195,8 @@ def logout():
 
 if __name__ == '__main__':
     # Running on 0.0.0.0 for EC2 access, but debug is OFF for safety
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=80, debug=False)
+
 
 
 
